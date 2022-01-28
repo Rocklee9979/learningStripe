a7a45b2a7805a4ef2dbe31967717135efa8e4185
Rocklee9979 <48349639+Rocklee9979@users.noreply.github.com>
2022-01-28 20:16:11 +1100

    dotenv working

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
+
 const express = require('express');
 const app = express();
+
+require('dotenv').config();
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -7,4 +13,4 @@ app.use(express.static('public'));
 
 app.listen(3000, () => {
     console.log("running on port 3000");
-})
+});
